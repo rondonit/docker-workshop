e8baf74ee240ff2891272c6da36308f1bfa20160
Gabriel Cruz <120282363+rondonit@users.noreply.github.com>
2026-01-15 20:21:13 +0000

    pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,9 +1,12 @@
 import sys
 import pandas as pd
 
-print("arguments:", sys.argv)
+print("arguments", sys.argv)
 
-month = sys.argv[1]
+day = int(sys.argv[1])
+print(f"Running pipeline for day {day}")
 
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
 
-print(f"hello pipeline, month is {month}")
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
